69bc5dc21f46d75867623bd10836e2ee37f97ff2
Vladyslav <69161293+AniFuryYT@users.noreply.github.com>
2022-05-29 12:55:51 +0300

    commit

---
+++ Lesson 20_homework finaly/js/main.js
@@ -0,0 +1,8 @@
+// Navigation
+$('.navbar .close').click(function() {
+	$('.nav').addClass('un-active-navbar');
+})
+
+$('.navbar .check-burger').click(function() {
+	$('nav').removeClass('un-active-navbar');
+})
