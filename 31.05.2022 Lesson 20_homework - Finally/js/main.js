46b7dafa1d35800a6c8caaab5381183c72d307f8
Vladyslav <animew1243@gmail.com>
2022-05-31 18:21:52 +0300

    commit

---
+++ 31.05.2022 Lesson 20_homework - Finally/js/main.js
@@ -0,0 +1,8 @@
+// Navigation
+$('.navbar .close').click(function() {
+	$('.menu').addClass('un-active-navbar');
+})
+
+$('.navbar .check-burger').click(function() {
+	$('.menu').removeClass('un-active-navbar');
+})
