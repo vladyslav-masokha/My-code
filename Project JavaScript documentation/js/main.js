3c650642e0c8d2ef2adfefe7b70eff95bb20a948
Vladyslav <animew1243@gmail.com>
2022-06-01 16:43:55 +0300

    commit

--- Project JavaScript documentation/js/main.js
+++ Project JavaScript documentation/js/main.js
@@ -1,9 +1,9 @@
 'use strict';
 // TODO: Navigation
-$('.navbar .close').click(function () {
+$('.close').click(function () {
 	$('.nav').addClass('un-active-navbar');
 });
 
-$('.navbar .check-burger').click(function () {
+$('.check-burger').click(function () {
 	$('.nav').removeClass('un-active-navbar');
 });
