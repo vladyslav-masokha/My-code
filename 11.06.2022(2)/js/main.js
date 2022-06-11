d6b50493b7d0a7f78c90d946353fc3511fc5e06e
Vladyslav <animew1243@gmail.com>
2022-06-11 23:01:56 +0300

    commit

---
+++ 11.06.2022(2)/js/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const inputValue = document.getElementById('input-value');
+const btnEl = document.getElementById('send-btn');
+const numbers = document.getElementsByClassName('numbers');
+
+const genNumbers = () => {
+	let num = [];
+	for (let i = 0; i < +inputValue.value; i++) {
+		num[i] = Math.round(Math.random() * 100);
+	}
+	return num;
+};
+
+const genElement = () => {
+	switch (inputValue.value) {
+		case '':
+			console.error('Error');
+			inputValue.placeholder = 'Error, enter text again!';
+			inputValue.className = 'for-error';
+			break;
+
+		default:
+			const spanEl = document.createElement('span');
+			const numTextEl = document.createTextNode(genNumbers());
+
+			spanEl.append(numTextEl);
+			numbers[0].append(spanEl);
+			break;
+	}
+	inputValue.value = '';
+};
+
+btnEl.onclick = () => {
+	genElement();
+};
