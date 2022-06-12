f5e1f117a0322e3b1cf271fe9c34d7ad105fede5
Vladyslav <animew1243@gmail.com>
2022-06-12 19:11:35 +0300

    commit

---
+++ 12.06.2022(3)/js/main.js
@@ -0,0 +1,34 @@
+// 'use strict';
+
+// const firstNumEl = document.getElementById('first-number').value;
+// const secondNumEl = document.getElementById('second-number').value;
+// const chooseOperationEl = document.getElementById('choose-op').value;
+// const btnEl1 = document.getElementById('btn-send1');
+// const btnEl2 = document.getElementById('btn-send2');
+// const btnEl3 = document.getElementById('btn-send3');
+
+// const firstBlockEl = document.getElementById('first');
+// const secondBlockEl = document.getElementById('second');
+// const thirdBlockEl = document.getElementById('third');
+// const resultBlockEl = document.getElementById('result');
+
+// btnEl1.onclick = () => {
+// 	firstBlockEl.style.display = 'none';
+// };
+
+// btnEl2.onclick = () => {
+// 	secondBlockEl.style.display = 'none';
+// };
+
+// btnEl3.onclick = () => {
+// 	thirdBlockEl.style.display = 'none';
+// 	resultEl();
+// };
+
+// const resultEl = () => {
+// 	const spanEl = document.createElement('span');
+// 	const result = `${firstNumEl} ${chooseOperationEl}`;
+// 	const resultText = document.createTextNode(result);
+// 	spanEl.append(resultText);
+// 	resultBlockEl.append(spanEl);
+// };
